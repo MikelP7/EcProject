da933a639b9c94bbd66f2a9021e91362bb21dc35
MikelP7 <90459959+MikelP7@users.noreply.github.com>
2022-05-04 09:57:03 +0200

    Add files via upload

---
+++ include/fondos.h
@@ -0,0 +1,7 @@
+/*-------------------------------------
+fondos.h
+-------------------------------------*/
+
+extern void visualizarPuerta();
+extern void visualizarPuertaAbierta();
+extern void visualizarIntro();
