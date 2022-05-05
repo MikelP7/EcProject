6ee92c727d3354b890016ce8a5bf75be3379e991
sendINUX <104916695+sendINUX@users.noreply.github.com>
2022-05-05 20:17:52 +0200

    Add files via upload

---
+++ include/fondos.h
@@ -0,0 +1,8 @@
+/*-------------------------------------
+fondos.h
+-------------------------------------*/
+
+extern void visualizarPuerta();
+extern void visualizarPuertaAbierta();
+extern void visualizarIntro();
+extern void visualizarMenu();
