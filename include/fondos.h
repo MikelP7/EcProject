f77e6506ad33f8b395e96f01112805588c1af94a
MikelP7 <90459959+MikelP7@users.noreply.github.com>
2022-05-05 12:58:16 +0200

    Delete include directory

--- include/fondos.h
+++
@@ -1,7 +0,0 @@
-/*-------------------------------------
-fondos.h
--------------------------------------*/
-
-extern void visualizarPuerta();
-extern void visualizarPuertaAbierta();
-extern void visualizarIntro();
