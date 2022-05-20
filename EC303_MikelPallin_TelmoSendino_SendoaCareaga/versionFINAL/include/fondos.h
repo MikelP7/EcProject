968ada8bcaffce1f5896c9cb032208577d816c5d
sendINUX <104916695+sendINUX@users.noreply.github.com>
2022-05-20 23:19:54 +0200

    Add files via upload

---
+++ EC303_MikelPallin_TelmoSendino_SendoaCareaga/versionFINAL/include/fondos.h
@@ -0,0 +1,13 @@
+/*-------------------------------------
+fondos.h
+-------------------------------------*/
+
+extern void visualizarIntro();
+extern void visualizarMenu();
+extern void visualizarMenu2();
+extern void visualizarFondo00();
+extern void visualizarFondo01();
+extern void visualizarFondo02();
+extern void visualizarFondo03();
+extern void visualizarFondo04();
+
